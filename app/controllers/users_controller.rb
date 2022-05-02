eb77de68f01aa5aa0a5cec5b4f0539a346a7a7ec
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-02 23:55:07 +0530

    Qr scan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if params['payid'].present?
       #search in existing contacts
       c = current_user.contacts.where(payid: params['payid']).first_or_create
-      c.avatar = params['payid'].capitalize
+      c.name = params['payid'].capitalize
       c.save!
       redirect_to contacts_path(c)
     end
