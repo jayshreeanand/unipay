a7e0613e04d422f16d0f1af6786a911ffde99754
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-03 03:44:35 +0530

    Fix home root

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'qr-scan', to: 'basic_qr_codes#index'
   get 'search', to: 'users#search'
-  root to: 'dashboard#index'
+  root to: 'pages#home'
   get 'home', to: 'pages#home'
 
   get 'profile', to: 'users#me'
