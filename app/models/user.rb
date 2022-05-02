3a0b3a2a7e8faa59b6428de6a8743980fbb23f52
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-03 00:48:53 +0530

    Gravatar for user profile pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :contacts, dependent: :destroy
 
+  before_save :add_gravatar
+
   def payid_full
     "#{payid}$insta-pay.me"
   end
 
   def qr_code_svg
     qrcode = RQRCode::QRCode.new("https://www.insta-pay.me/search?payid=#{payid}")
+  end
+
+  def add_gravatar
+    if !avatar.present?
+      email = self.email.downcase
+      email_hash = Digest::MD5.hexdigest(email)
+      self.avatar = "https://www.gravatar.com/avatar/#{email_hash}/?d=identicon"
+    end
+  end
+
+  def avatar_url
+    avatar || 'avatar.png'
   end
 end
