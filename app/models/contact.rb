4afd43b1eb4058cc327cb79a4f2d22a546255cce
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-02 23:14:21 +0530

    Added contact seed data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,8 @@ class Contact < ApplicationRecord
   validates :name, presence: true
   validates :payid, presence: true
   enum kind: { user: 0, business: 1, charity: 2}
+
+  def payid_full
+    "#{payid}$insta-pay.me"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+images = [
+  'https://randomuser.me/api/portraits/men/75.jpg',
+]
+
+names = ['Kingston',
+'Vincent',
+'Brett',
+'Hunter',
+'Franco',
+'Messiah',
+'Jaydin',
+'Jordyn',
+'Jakobe',
+'Zaire',
+'Terrance',
+'Kasey'
+]
+names.each do |name|
+  c = Contact.new
+  c.name = name
+  c.avatar = "https://randomuser.me/api/portraits/men/#{rand(1..100)}.jpg"
+  c.payid = name.downcase
+  c.save!
+
+names_new = [
+  'Lia'
+'Genesis'
+'Adison'
+'Piper'
+'Akira'
+'Elsie'
+]
+
+names_new.each do |name|
+  c = Contact.new
+  c.name = name
+  c.avatar = "https://randomuser.me/api/portraits/women/#{rand(1..100)}.jpg"
+  c.payid = name.downcase
+  c.save!
