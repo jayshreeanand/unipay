6340e73d320063b10c0dc3505c357cc728fcc3cd
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-02 19:09:51 +0530

    Added payid to users

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_125427) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "payid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
