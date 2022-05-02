30ef87f07f6c5f1fe88316f3d5246289658f0d75
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-02 17:40:38 +0530

    QR scan route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'basic-qr-code-reader', to: 'basic_qr_codes#index'
-  
+  get 'qr-scan', to: 'basic_qr_codes#index'
+
   root to: 'pages#home'
 end
