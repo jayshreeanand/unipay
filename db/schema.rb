041fb22f27abed52a707f0ec91bae309d0c20891
Jayshree Anandakumar <ping.jayshree@gmail.com>
2022-05-02 19:27:49 +0530

    Contacts migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_125427) do
+ActiveRecord::Schema.define(version: 2020_08_02_135701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.bigint "user_id_id"
+    t.string "name"
+    t.text "description"
+    t.string "payid"
+    t.integer "type"
+    t.string "avatar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id_id"], name: "index_contacts_on_user_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
